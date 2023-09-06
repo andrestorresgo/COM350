069a2cd0950f5928f80cac0e87136f7ccc1bb6da
andrestorresgo <andytorresgonz@gmail.com>
2023-09-05 23:36:48 -0400

    Update grupos.php

--- webpage/grupos.php
+++ webpage/grupos.php
@@ -20,49 +20,46 @@
         <?php
         include 'config.php';
         
-        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-            $subject_id = $row["id_materia"];
+        $sql_subjects = "SELECT DISTINCT id_materia FROM horarios";
+        $result_subjects = $pdo->query($sql_subjects);
+
+        while ($subject_row = $result_subjects->fetch(PDO::FETCH_ASSOC)) {
+            $subject_id = $subject_row["id_materia"];
         
             // Fetch schedule data for the current subject with a JOIN
             $sql = "SELECT horarios.*, aulas.aula AS classroom_name
                     FROM horarios
                     INNER JOIN aulas ON horarios.id_aula = aulas.id
                     WHERE horarios.id_materia = $subject_id";
-            $result = $db->query($sql);
+            $result = $pdo->query($sql);
         
-            if ($result->num_rows > 0) {
-                echo "<div class='container mt-5'>";
-                echo "<h1 class='text-center mb-4'>Class Schedule for Subject $subject_id</h1>";
-                echo "<table class='table table-bordered'>";
-                echo "<thead>";
+            echo "<div class='container mt-5'>";
+            echo "<h1 class='text-center mb-4'>Horario para Grupo $subject_id</h1>";
+            echo "<table class='table table-bordered'>";
+            echo "<thead>";
+            echo "<tr>";
+            echo "<th scope='col'>Hora</th>";
+            echo "<th scope='col'>Materia</th>";
+            echo "<th scope='col'>Aula</th>";
+            echo "<th scope='col'>Docente</th>";
+            echo "<th scope='col'>Dia</th>";
+            echo "</tr>";
+            echo "</thead>";
+            echo "<tbody>";
+    
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
                 echo "<tr>";
-                echo "<th scope='col'>Time</th>";
-                echo "<th scope='col'>Subject</th>";
-                echo "<th scope='col'>Classroom</th>";
-                echo "<th scope='col'>Instructor</th>";
-                echo "<th scope='col'>Day</th>";
+                echo "<td>" . $row["hora_entrada"] . " - " . $row["hora_salida"] . "</td>";
+                echo "<td>" . $row["id_materia"] . "</td>";
+                echo "<td>" . $row["classroom_name"] . "</td>"; // Display classroom name from the JOIN
+                echo "<td>" . $row["id_docente"] . "</td>";
+                echo "<td>" . $row["dia_de_semana"] . "</td>";
                 echo "</tr>";
-                echo "</thead>";
-                echo "<tbody>";
-        
-                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    echo "<tr>";
-                    echo "<td>" . $row["hora_entrada"] . " - " . $row["hora_salida"] . "</td>";
-                    echo "<td>" . $row["id_materia"] . "</td>";
-                    echo "<td>" . $row["classroom_name"] . "</td>"; // Display classroom name from the JOIN
-                    echo "<td>" . $row["id_docente"] . "</td>";
-                    echo "<td>" . $row["dia_de_semana"] . "</td>";
-                    echo "</tr>";
-                }
-        
-                echo "</tbody>";
-                echo "</table>";
-                echo "</div>";
-            } else {
-                echo "<div class='container mt-5'>";
-                echo "<p>No schedule data available for Subject $subject_id.</p>";
-                echo "</div>";
             }
+    
+            echo "</tbody>";
+            echo "</table>";
+            echo "</div>";
         }
         ?>
     </div>
